0b2a716b9e08c0a9298e5cdade6e81ab0cc0f461
andresperezceron <aperezlevi@gmail.com>
2019-05-02 09:50:08 +0200

    style

--- src/routes.js
+++ src/routes.js
@@ -17,10 +17,10 @@ router.get("/users", function(req, res) {
 });
 
 router.post("/login", function(req, res) {
-    const name = req.body.name;
+    const email = req.body.email;
     const password = req.body.password;
     const db = new sqlite3.Database("./db/stdb.db");
-    const query = "SELECT COUNT(*) AS count FROM users u WHERE u.login='" + name + "' AND u.password='" + password + "';";
+    const query = "SELECT COUNT(*) AS count FROM users u WHERE u.login='" + email + "' AND u.password='" + password + "';";
     db.get(query, (error, row) => res.json(row.count > 0));
 });
 module.exports = router;
